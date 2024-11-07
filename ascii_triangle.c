9891d03d3e8c8c85c0d3364a5100d6e58b5d2d0a
krzysztof turski <krzysztof.turski40@gmail.com>
2024-11-07 11:21:54 +0100

    fixbag

--- ascii_triangle.c
+++ ascii_triangle.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_ROWS 15 // Number of rows in the triangle
+#define CHARACTERS 20  // Number of rows for the pattern
 
 int main(void) {
-    char result[500] = "";  // Array to store the resulting string
+    int i, row, j;
+    char ch;
+    char letter = 'A';
+    char letter3 = 'A';
 
-    for (int row = 0; row < MAX_ROWS; row++) {
-        char line[50] = "";  // Stores the current line of the pattern
+    // Loop through each row
+    for (row = 0; row < CHARACTERS; row++) {
 
-        // Adding letters in ascending order
-        for (int i = 0; i <= row; i++) {
-            char letter[2];
-            sprintf(letter, "%c", 'A' + i);
-            strcat(line, letter);
+        // Print leading spaces for alignment
+        for (i = 0; i <= 18 - row; i++) {
+            printf(" ");
         }
 
-        // Adding letters in descending order
-        for (int i = row - 1; i >= 0; i--) {
-            char letter[2];
-            sprintf(letter, "%c", 'A' + i);
-            strcat(line, letter);
+        // Print ascending letters
+        for (i = 0, ch = letter; i <= row; i++, ch++) {
+            printf("%c", ch);
         }
 
-        // Adding the line to the result with a space
-        strcat(result, line);
-        if (row < MAX_ROWS - 1) {
-            strcat(result, " ");
+        // Print descending letters
+        for (i = 0, ch = letter3 - 1; i < row; i++, ch--) {
+            printf("%c", ch);
         }
+
+        // Increment the starting letter for the next row
+        letter3++;
+
+        // Move to the next line after finishing the current row
+        printf("\n");
     }
-
-    printf("%s\n", result); // Displaying the resulting string
 
     return 0;
 }
