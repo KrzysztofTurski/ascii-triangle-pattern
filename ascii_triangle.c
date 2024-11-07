ebee26c0488df62eb929bd06049be8fd280f151b
krzysztof turski <krzysztof.turski40@gmail.com>
2024-11-07 08:12:49 +0100

    Implement letter triangle pattern generation

---
+++ ascii_triangle.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_ROWS 5 // Number of rows in the triangle
+
+int main(void) {
+    char result[500] = "";  // Array to store the resulting string
+
+    for (int row = 0; row < MAX_ROWS; row++) {
+        char line[50] = "";  // Stores the current line of the pattern
+
+        // Adding letters in ascending order
+        for (int i = 0; i <= row; i++) {
+            char letter[2];
+            sprintf(letter, "%c", 'A' + i);
+            strcat(line, letter);
+        }
+
+        // Adding letters in descending order
+        for (int i = row - 1; i >= 0; i--) {
+            char letter[2];
+            sprintf(letter, "%c", 'A' + i);
+            strcat(line, letter);
+        }
+
+        // Adding the line to the result with a space
+        strcat(result, line);
+        if (row < MAX_ROWS - 1) {
+            strcat(result, " ");
+        }
+    }
+
+    printf("%s\n", result); // Displaying the resulting string
+
+    return 0;
+}
